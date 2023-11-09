5f804fe97a57e8cbb11556b6cc88d3f4f37fb657
escowin <edwin@escowinart.com>
2023-11-09 11:50:10 -0600

    outlined server logic

--- server/server.js
+++ server/server.js
@@ -1,2 +1,12 @@
+// Dependencies
 const path = require("path")
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+// Imported variables (schemas, auth middleware, db connection)
+
+// Server configuration
+// Initialize port connectioon for production & local machine
+// Initialize new Apollo Server instance
+// Initialize Express server & middleware
+// IF deployed to production, use client side build for frontend code 
+// Initialize & call Apollo Server with GraphQL schema parameters.
